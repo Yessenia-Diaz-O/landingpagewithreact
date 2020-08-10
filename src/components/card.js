5c223457a57da04af51c5de6ba928501464d7232
andrequera <andrequera@gmail.com>
2020-08-10 00:16:14 -0400

    guardando mis cambios

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props)
     return (<>
+
+
+        <div className="container">
+            <div className="row">
+                {
+                    props.arreglodecartas.map((c, i) => {
+                        console.log(c)
+                        return (
+                            <div className="col-md-3">
+                                <div className="card">
+                                    <img className="card-img-top" src="..." alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{c.titulo}</h5>
+                                        <p className="card-text">{c.texto}</p>
+                                        <a href="#" classNameName="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+
 
 
 
--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,34 @@
 import React from "react";
+import Card from "../components/card";
+
+
 
 const Home = () => {
+    const cartas = [
+        {
+            titulo: "Andrea cartas 1",
+            texto: "andrea texto 1",
+        },
+        {
+            titulo: "Andrea cartas 2",
+            texto: "andrea texto 2",
+        },
+        {
+            titulo: "Andrea cartas 3",
+            texto: "andrea texto 3",
+        },
+        {
+            titulo: "Andrea cartas 4",
+            texto: "andrea texto 4",
+        },
+    ]
     return (<>
 
         {/* navbar */}
         {/* jumbo */}
         {/* card */}
+        <Card arreglodecartas= {cartas} />
+        
         {/* footer */}
         
     </>)
